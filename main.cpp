ba7ae56b3971af86bd6591542f43cb4552577b55
Bernard Rouhi <Bernardrouhi@users.noreply.github.com>
2022-05-18 02:15:01 +0800

    new feature
    - added Velocity and Gravity
    - added Jump Key (Space)
    - added Movement keys (A and D)

--- main.cpp
+++ main.cpp
@@ -7,12 +7,21 @@ int main()
 
     // initiate window
     InitWindow(Window_W,Window_H,"Platform Movement");
+    
+    // World Property
+    const int gravity{1};
 
     // character size
     const int Char_W{50};
-    const int Char_H{80};
+    const int Char_H{50};
 
     int Char_PosY{Window_H-Char_H};
+    int Char_PosX{Window_W/2};
+    int Char_Velocity{0};
+    const int Char_JumpVelocity{-22};
+    const int Char_Speed{10};
+
+    bool isInAir{false};
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
@@ -21,7 +30,53 @@ int main()
    
         ClearBackground(WHITE);
 
-        DrawRectangle(Window_W/2,Char_PosY, Char_W, Char_H, SKYBLUE);
+        if (Char_PosY >= Window_H - Char_H)
+        {
+            Char_Velocity = 0;
+            isInAir = false;
+        }
+        else
+        {
+            // Apply gravity
+            Char_Velocity += gravity;
+            isInAir = true;
+        }
+
+        // Jump
+        if(IsKeyPressed(KEY_SPACE) && !isInAir)
+        {
+            // add Velocity
+            Char_Velocity += Char_JumpVelocity;
+        }
+
+        // Movement
+        if(IsKeyDown(KEY_A))
+        {
+            if(Char_PosX > 0)
+            {
+                Char_PosX -= Char_Speed;
+            }
+            else
+            {
+                Char_PosX = 0;
+            }
+        }
+        if(IsKeyDown(KEY_D))
+        {
+            if (Char_PosX < Window_W-Char_W)
+            {
+                Char_PosX += Char_Speed;
+            }
+            else
+            {
+                Char_PosX = Window_W-Char_W;
+            }
+        }
+
+        // update character position
+        Char_PosY += Char_Velocity;
+
+        DrawRectangle(Char_PosX,Char_PosY, Char_W, Char_H, SKYBLUE);
 
         EndDrawing();
     }
